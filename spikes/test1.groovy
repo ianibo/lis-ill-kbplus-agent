df78b0362082d7953b3c801860d2e7e362736151
Ian Ibbotson <ian.ibbotson@k-int.com>
2014-07-31 09:55:24 +0100

    added spikes

---
+++ spikes/test1.groovy
@@ -0,0 +1,21 @@
+
+
+def search_url = "https://www.jiscmail.ac.uk/cgi-bin/webadmin"
+def params = [
+  '9':'A',
+  'X':'',
+  'Y':'',
+  '_charset_':'UTF-8',
+  'z':'4',
+  'q':'*',
+  '0':'S',
+  's':'',
+  'f':'',
+  'a':'',
+  'b':'',
+  'I':'-3',
+  'd':'No Match;Match;Matches',
+  'J':'on',
+  '1:'LIS-ILL',
+  'L':'LIS-ILL',
+]
